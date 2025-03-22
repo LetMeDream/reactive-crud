0e9fa1629a1bfe0296586aa3feffe0fb68e04098
mariann <cesinelisa@gmail.com>
2025-03-22 18:07:26 -0500

    chore (Form.js): crear archivo Form.js dentro de carpeta constants y variable editAttributesOptions

---
+++ src/constants/Form.js
@@ -0,0 +1,17 @@
+export const editAttributesOption = [
+        {
+        'id': 'product-name',
+        'product-name': 'Nombre del Producto'
+        },
+        {
+        'id': 'product-value',
+        'product-name': 'Valor del Producto'
+        },
+        {'product-name': 'Existencia',
+          'id': 'product-existance'
+        },
+        { 'id': 'product-url-image',
+          'product-name': 'Url Imagen'
+        }
+     ];
+    
