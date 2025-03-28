81cb6f187afac6fb57bf2b310cb9dd814ef5a75d
mariann <cesinelisa@gmail.com>
2025-03-28 17:06:02 -0500

    feat(EditProducts.jsx): implementacion de logica para editar y actualizar un producto en el array products.

--- src/components/Sections/EditProduct.jsx
+++ src/components/Sections/EditProduct.jsx
@@ -8,6 +8,8 @@ import { useForm, FormProvider } from "react-hook-form"
 import { yupResolver } from '@hookform/resolvers/yup'
 import { editAttributesOption } from "../../constants/Form"
 import { editProductFormSchema } from "../../constants/Schemas"
+import { errorToast } from "../../helpers/toasts"
+import { saveToLocalStorage } from "../../helpers/localStorage"
 
 const EditProduct = ({
   xs,
@@ -15,7 +17,8 @@ const EditProduct = ({
   md, 
   lg, 
   classnames = '',
-  products
+  products,
+  setProducts
 }) => {
 
  const methods = useForm({
@@ -24,8 +27,27 @@ const EditProduct = ({
 
   const {register, handleSubmit, trigger} = methods;
 
-  const onSubmit = async () => {
-    console.log(await trigger())
+  const onSubmit = async (formValues) => {
+    if (await trigger()) {
+      const selectedProduct = products.find(
+      (product) => product.id === formValues['product-name']);
+      const selectedProperty = formValues?.['product-attribute'];
+      const newValue = formValues?.['product-new-value'];
+      // Crear un nuevo objeto con la clave actualizada
+      const updatedProduct = { ...selectedProduct, [selectedProperty]: newValue };
+      // Actualizar el array de productos reemplazando el producto actualizado
+      const updatedProducts = products.map((product) =>
+      product.id === updatedProduct.id ? updatedProduct : product);
+      // Actualizar el estado y guardar en localStorage
+      setProducts(updatedProducts);
+      saveToLocalStorage("products", updatedProducts);
+      console.log("Productos actualizados:", updatedProducts);
+    } else {
+      errorToast({
+        title: 'Ups!',
+        content: 'Revise los errores del formulario'
+      })
+    }
   };
 
   return (
@@ -83,7 +105,8 @@ EditProduct.propTypes = {
   lg: PropTypes.number,
   classnames: PropTypes.string,
   products: PropTypes.array,
-  claves: PropTypes.array
+  claves: PropTypes.array,
+  setProducts: PropTypes.func
 }
 
 
